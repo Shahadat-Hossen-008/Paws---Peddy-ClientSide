4959a73939094781aefe580347dbf90b4a015f75
Shahadat-Hossen-008 <penaxavier078@gmail.com>
2025-01-28 12:42:38 +0600

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function App () {
+  return (
+    <div className='text-6xl'>App
+    <button className="btn btn-xs">Xsmall</button>
+<button className="btn btn-sm">Small</button>
+<button className="btn">Medium</button>
+<button className="btn btn-lg">Large</button>
+<button className="btn btn-xl">Xlarge</button></div>
+  )
+}
+
+export default App
